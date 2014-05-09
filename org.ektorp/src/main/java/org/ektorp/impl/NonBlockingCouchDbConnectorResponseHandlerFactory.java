aa25b8ded91328db44f931eef2de20046fc3f38f
Yann Robert <yann.robert@anantaplex.fr>
2014-05-09 18:10:56 +0200

    added comments on new class NonBlockingCouchDbConnectorResponseHandlerFactory

--- org.ektorp/src/main/java/org/ektorp/impl/NonBlockingCouchDbConnectorResponseHandlerFactory.java
+++ org.ektorp/src/main/java/org/ektorp/impl/NonBlockingCouchDbConnectorResponseHandlerFactory.java
@@ -5,6 +5,20 @@ import org.ektorp.CouchDbConnector;
 import org.ektorp.http.ClassInstanceResponseHandler;
 import org.ektorp.http.ResponseCallback;
 
+/**
+ * In a multi-threaded environment, when multiple threads are using Jackson as the very same time, threads can be blocked by synchronization happening into Jackson internal classes (regarding caching of class definitions).
+ * In some case, when many threads are used to load from or save to the database, this can badly affect performances.
+ * 
+ * Using this class, a different ObjectMapper instance will be used by each Thread. This permits to avoid the synchronization problem in Jackson.
+ * ObjectMappers instances are maintained in a ThreadLocal, so that is can be reused. (It is recommended to reuse threads, avoid creating new Threads)
+ * 
+ * For single-threaded application, this may also be interesting to use it, as the ResponseHandler instances are pooled, avoid to create a new temporary Object on each invocation.
+ * 
+ * For most applications, this should therefore be better than DefaultCouchDbConnectorResponseHandlerFactory.
+ * 
+ * usage : stdCouchDbConnector.setCouchDbConnectorResponseHandlerFactory(new NonBlockingCouchDbConnectorResponseHandlerFactory(stdCouchDbConnector));
+ * 
+ */
 public class NonBlockingCouchDbConnectorResponseHandlerFactory implements CouchDbConnectorResponseHandlerFactory {
 
 	private final ThreadLocal<ClassInstanceResponseHandler> classInstanceResponseHandlerThreadLocal = new ThreadLocal<ClassInstanceResponseHandler>() {
